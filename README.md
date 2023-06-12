b8927cd91f0f60171d200b1fd6cb9f59dcb47bce
Chris Pucknell <chris.pucknell@gmail.com>
2023-06-12 19:27:02 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Valheim Pet Name Generator Project
+A quick SAP for generating random names.
+
+Visit this page [here](#)
+
+## Technology
+- React
+- Typescript
+- Vite
+
+## Local Development Instructions
+1. Clone this repo
+2. cd 'valheim-pet-name-gen'
+3. npm i
+4. npm run dev
---
+++ src/App.tsx
@@ -0,0 +1,151 @@
+import "./styles.css";
+import { default as Button } from "./Button";
+import React, { useState } from "react";
+import valheimLogo from "./images/logo.webp";
+function App() {
+    const petNames = [
+        "Buddy",
+        "Max",
+        "Charlie",
+        "Cooper",
+        "Rocky",
+        "Bear",
+        "Tiger",
+        "Leo",
+        "Milo",
+        "Simba",
+        "Oscar",
+        "Toby",
+        "Felix",
+        "Gizmo",
+        "Lucky",
+        "Winston",
+        "Zeus",
+        "Shadow",
+        "Coco",
+        "Oreo",
+        "Jasper",
+        "Sammy",
+        "Oliver",
+        "Spike",
+        "Harley",
+        "Chase",
+        "Rusty",
+        "Luna",
+        "Molly",
+        "Daisy",
+        "Lucy",
+        "Sadie",
+        "Maggie",
+        "Stella",
+        "Penny",
+        "Bailey",
+        "Ruby",
+        "Rosie",
+        "Zoe",
+        "Bella",
+        "Lola",
+        "Sophie",
+        "Chloe",
+        "Lily",
+        "Mia",
+        "Maddie",
+        "Roxy",
+        "Princess",
+        "Emma",
+        "Cleo",
+        "Hazel",
+        "Annie",
+        "Millie",
+        "Grace",
+        "Nala",
+        "Pepper",
+        "Callie",
+        "Willow",
+        "Ginger",
+        "Zara",
+        "Piper",
+        "Kitty",
+        "Cupcake",
+        "Cookie",
+        "Biscuit",
+        "Honey",
+        "Pumpkin",
+        "Sugar",
+        "Peanut",
+        "Buttercup",
+        "Doodle",
+        "Snickerdoodle",
+        "Noodle",
+        "Snickers",
+        "Jellybean",
+        "Marshmallow",
+        "Pickle",
+        "Peaches",
+        "Blueberry",
+        "Caramel",
+        "Cotton",
+        "Candy",
+        "Cinnamon",
+        "S'mores",
+        "Tootsie",
+        "Sushi",
+        "Waffles",
+        "Nugget",
+        "Popcorn",
+        "Muffin",
+        "Pudding",
+        "Sprinkles",
+        "Whiskers",
+        "Socks",
+        "Paws",
+        "Furry",
+        "Whiskers",
+    ];
+
+    const [petName, setPetName] = useState("Blob");
+    const [buttonText, setButtonText] = useState("Copy");
+
+    function randomIntFromInterval(min: number, max: number) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    function handleCopy(){
+    navigator.clipboard.writeText(petName);
+    setButtonText("Copied!")
+    setTimeout(()=>setButtonText("Copy"),2000)
+    }
+
+    const handleOnClick = () => {
+        const rndInt = randomIntFromInterval(0, petNames.length - 1);
+        const newName = petNames[rndInt];
+        setPetName(newName);
+    };
+
+    return (
+        <div className="main">
+            <div className="panel">
+                <div className="top">
+                    <img className="logo" src={valheimLogo} alt="logo"></img>
+                    <h2>Generate a Random Pet Name</h2>
+                </div>
+                <span className="glow">{petName}</span>
+                <div className="button-grid">
+                    <Button
+                        variant='primary'
+                        onClick={() => handleOnClick()}
+                    >
+                        Generate Name
+                    </Button>
+                    <Button
+                        onClick={() => handleCopy()}
+                    >
+                        {buttonText}
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
